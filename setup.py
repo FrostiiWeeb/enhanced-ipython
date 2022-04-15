f15add2e328a2300de2d73bd23661528cc3de4bc
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-04-15 21:50:34 +0200

    Add IPython basics.

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
-setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
-    long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
+setup(
+    name="enhanced-ipython",
+    version="0.0.1",
+    description="An enhanced IPython shell.",
+    long_description=readme,
+    author="Alex Hutz",
+    author_email="frostiiweeb@gmail.com",
+    url="https://github.com/FrostiiWeeb/enhanced-ipython",
+    license=license,
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": ["eipython=eipython.__main__:run"],
+    },
+)
